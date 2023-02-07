b8ca24aec7cc3092b53b561a0f0322fa38a74246
illika <steve.milan7@gmail.com>
2023-02-07 13:08:23 -0500

    Directive - @Input | ElementRef - HtmlElement

--- src/app/shared/directives/error-msg.directive.ts
+++ src/app/shared/directives/error-msg.directive.ts
@@ -1,17 +1,25 @@
-import { Directive, OnInit } from '@angular/core';
+import { Directive, ElementRef, Input, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[errorMsg]'
 })
 export class ErrorMsgDirective implements OnInit {
 
-  constructor() {
-    console.log("directiva");
-    
-   }
+  htmlElement: ElementRef<HTMLElement>;
+
+  @Input() color: string = "red";
+
+  constructor(private el: ElementRef<HTMLElement>) {
+    this.htmlElement = el;
+  }
+
   ngOnInit(): void {
     console.log("OnInit DIrective");
-    
+    this.setColor();
+  }
+
+  setColor(): void {
+    this.htmlElement.nativeElement.style.color = this.color;
   }
 
 }
